1a336a5361c22c9f8092cbeb93d9496da3fac550
Walaa942 <wabouzidan@myseneca.ca>
2026-04-08 13:26:59 -0400

    Modify send_email function to include game results

--- email_system.py
+++ email_system.py
@@ -1,12 +1,31 @@
 import smtplib
 from email.mime.text import MIMEText
 
-def send_email(receiver_email, subject, body):
+def send_email(receiver_email, player, score, hints, time_taken):
     smtp_server = 'smtp.gmail.com'
     port = 587
 
     sender_email = 'your_email@gmail.com'
     sender_password = 'your_app_password'
+
+    subject = "Escape Room Game Result"
+
+    body = f"""
+Hello {player},
+
+Here are your Escape Room results:
+
+Puzzles solved: {score}
+Hints used: {hints}
+Time taken: {round(time_taken, 2)} seconds
+
+Better performance means:
+- More puzzles solved
+- Fewer hints used
+- Less time taken
+
+Thanks for playing!
+"""
 
     message = MIMEText(body, 'plain')
     message['From'] = sender_email
